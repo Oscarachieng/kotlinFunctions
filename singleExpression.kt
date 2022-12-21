d486b9533e8a0da4a8afff97e7b896634ca5294f
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-12-21 07:06:22 +0000

    Adds singleExpression.kt file

---
+++ singleExpression.kt
@@ -0,0 +1,10 @@
+ // Single expression functions
+
+fun triangleArea(height: Double, width: Double ) :Double {
+  return width * height/2
+}
+
+fun main () {
+  println("Area of the Triangle")
+  triangleArea(20.3,10.5)
+}
