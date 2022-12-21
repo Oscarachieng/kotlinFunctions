94c47247047fe254c10d37a62454bce24137a278
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-12-21 07:24:01 +0000

    adds a game play console application

---
+++ game.kt
@@ -0,0 +1,19 @@
+fun getPoints(basePoint: Int, boost: Int) :Int {
+  return basePoint * boost
+}
+
+
+fun main() {
+    var score = 0;
+println(score)
+    score += getPoints(10, 1)
+println(score)
+    score += getPoints(20, 2)
+println(score)
+    score += getPoints(-10, 1)
+println(score)
+    score += getPoints(5, 3)
+println(score)
+    score += getPoints(-15, 2)
+}
+
